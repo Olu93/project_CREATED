3230617acd47ec8569e52e00a21ef78dbb021a9a
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-01-12 21:55:08 +0100

    Ref: eval to class

--- src/thesis_predictors/helper/evaluation.py
+++ src/thesis_predictors/helper/evaluation.py
@@ -9,11 +9,13 @@ from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_sc
 import pandas as pd
 from tqdm import tqdm
 import textdistance
+from tensorflow.keras import Model
+from src.thesis_readers.readers.AbstractProcessLogReader import AbstractProcessLogReader
 from ..helper.constants import NUMBER_OF_INSTANCES, SEQUENCE_LENGTH
 from ..models.lstm import SimpleLSTMModelOneWay
 from ..models.transformer import TransformerModelOneWay
 from thesis_readers.readers.BPIC12LogReader import BPIC12LogReader
- 
+
 STEP1 = "Step 1: Iterate through data"
 STEP2 = "Step 2: Compute Metrics"
 STEP3 = "Step 3: Save results"
@@ -21,37 +23,82 @@ FULL = 'FULL'
 symbol_mapping = {index: char for index, char in enumerate(set([chr(i) for i in range(1, 3000) if len(chr(i)) == 1]))}
 
 
-def results_by_instance_seq2seq(idx2vocab, start_id, end_id, test_dataset, model, mode='weighted'):
-    print("Start results by instance evaluation")
-    print(STEP1)
-    X_test, y_test = test_dataset
-    y_test = y_test.astype(int)
-    print(STEP2)
-    eval_results = []
-    y_pred = model.predict(X_test[0] if len(X_test) == 1 else X_test).argmax(axis=-1).astype(np.int32)
-    iterator = enumerate(zip(X_test[0], y_test, y_pred))
-    for idx, (row_x_test, row_y_test, row_y_pred) in tqdm(iterator, total=len(y_test)):
-        row_x_test = row_x_test.astype(np.int32)
-        first_word_test = np.max([np.argmin(row_y_test == 0), 1])
-        first_word_pred = np.max([np.argmin(row_y_pred == 0), 1])
-        first_word_x = np.argmin(row_x_test == 0)
-        longer_sequence_start = min([first_word_test, first_word_pred])
-        instance_result = {
-            "trace": idx,
-            f"full_{SEQUENCE_LENGTH}": len(row_y_test)-first_word_test,
-            f"input_x_{SEQUENCE_LENGTH}": len(row_x_test)-first_word_x,
-            f"true_y_{SEQUENCE_LENGTH}": len(row_y_test)-first_word_test,
-            f"pred_y_{SEQUENCE_LENGTH}": len(row_y_pred)-first_word_pred,
-        }
-        instance_result.update(compute_traditional_metrics(mode, row_y_test[longer_sequence_start:], row_y_pred[longer_sequence_start:]))
-        instance_result.update(compute_sequence_metrics(row_y_test[first_word_test:], row_y_pred[-first_word_pred:]))
-        instance_result.update(compute_decoding(idx2vocab, row_y_pred[first_word_pred:], row_y_test[first_word_test:], row_x_test[first_word_x:]))
-        eval_results.append(instance_result)
+class Evaluation(object):
+    def __init__(self, reader: AbstractProcessLogReader, model: Model) -> None:
+        super().__init__()
+        self.reader = reader
+        self.model = model
+        self.idx2vocab = self.reader.idx2vocab
 
-    results = pd.DataFrame(eval_results)
-    print(STEP3)
-    print(results)
-    return results
+    def results_by_instance_seq2seq(self, test_dataset, mode='weighted'):
+        print("Start results by instance evaluation")
+        print(STEP1)
+        X_test, y_test = test_dataset
+        y_test = y_test.astype(int)
+        print(STEP2)
+        eval_results = []
+        y_pred = self.model.predict(X_test[0] if len(X_test) == 1 else X_test).argmax(axis=-1).astype(np.int32)
+        iterator = enumerate(zip(X_test[0], y_test, y_pred))
+        for idx, (row_x_test, row_y_test, row_y_pred) in tqdm(iterator, total=len(y_test)):
+            row_x_test = row_x_test.astype(np.int32)
+            first_word_test = np.max([np.argmin(row_y_test == 0), 1])
+            first_word_pred = np.max([np.argmin(row_y_pred == 0), 1])
+            first_word_x = np.argmin(row_x_test == 0)
+            longer_sequence_start = min([first_word_test, first_word_pred])
+            instance_result = {
+                "trace": idx,
+                f"full_{SEQUENCE_LENGTH}": len(row_y_test) - first_word_test,
+                f"input_x_{SEQUENCE_LENGTH}": len(row_x_test) - first_word_x,
+                f"true_y_{SEQUENCE_LENGTH}": len(row_y_test) - first_word_test,
+                f"pred_y_{SEQUENCE_LENGTH}": len(row_y_pred) - first_word_pred,
+            }
+            instance_result.update(self.compute_traditional_metrics(mode, row_y_test[longer_sequence_start:], row_y_pred[longer_sequence_start:]))
+            instance_result.update(self.compute_sequence_metrics(row_y_test[first_word_test:], row_y_pred[-first_word_pred:]))
+            instance_result.update(self.compute_decoding(row_y_pred[first_word_pred:], row_y_test[first_word_test:], row_x_test[first_word_x:]))
+            eval_results.append(instance_result)
+
+        results = pd.DataFrame(eval_results)
+        print(STEP3)
+        print(results)
+        return results
+
+    def compute_traditional_metrics(self, mode, row_y_test_zeros, row_y_pred_zeros):
+        return {
+            "acc": accuracy_score(row_y_pred_zeros, row_y_test_zeros),
+            "recall": recall_score(row_y_pred_zeros, row_y_test_zeros, average=mode, zero_division=0),
+            "precision": precision_score(row_y_pred_zeros, row_y_test_zeros, average=mode, zero_division=0),
+            "f1": f1_score(row_y_pred_zeros, row_y_test_zeros, average=mode, zero_division=0),
+        }
+
+
+    def compute_sequence_metrics(self, true_seq, pred_seq):
+        true_seq_symbols = "".join([symbol_mapping[idx] for idx in true_seq])
+        pred_seq_symbols = "".join([symbol_mapping[idx] for idx in pred_seq])
+        dict_instance_distances = {
+            "levenshtein": textdistance.levenshtein.normalized_similarity(true_seq_symbols, pred_seq_symbols),
+            "damerau_levenshtein": textdistance.damerau_levenshtein.normalized_similarity(true_seq_symbols, pred_seq_symbols),
+            "local_alignment": textdistance.smith_waterman.normalized_similarity(true_seq_symbols, pred_seq_symbols),
+            "global_alignment": textdistance.needleman_wunsch.normalized_similarity(true_seq_symbols, pred_seq_symbols),
+            "emph_start": textdistance.jaro_winkler.normalized_similarity(true_seq_symbols, pred_seq_symbols),
+            "longest_subsequence": textdistance.lcsseq.normalized_similarity(true_seq_symbols, pred_seq_symbols),
+            "longest_substring": textdistance.lcsstr.normalized_similarity(true_seq_symbols, pred_seq_symbols),
+            "overlap": textdistance.overlap.normalized_similarity(true_seq_symbols, pred_seq_symbols),
+            "entropy": textdistance.entropy_ncd.normalized_similarity(true_seq_symbols, pred_seq_symbols),
+        }
+        return dict_instance_distances
+
+
+    def compute_decoding(self, row_y_pred, row_y_test, row_x_test):
+        x_convert = [f"{i:03d}" for i in row_x_test]
+        return {
+            "input": " | ".join(["-".join(x_convert[:lim + 1]) for lim in range(len(x_convert))]),
+            "true_encoded": " -> ".join([f"{i:03d}" for i in row_y_test]),
+            "pred_encoded": " -> ".join([f"{i:03d}" for i in row_y_pred]),
+            "true_encoded_with_padding": " -> ".join([f"{i:03d}" for i in row_y_test]),
+            "pred_encoded_with_padding": " -> ".join([f"{i:03d}" for i in row_y_pred]),
+            "true_decoded": " -> ".join([self.idx2vocab[i] for i in row_y_test]),
+            "pred_decoded": " -> ".join([self.idx2vocab[i] for i in row_y_pred]),
+        }
 
 def results_by_instance(idx2vocab, start_id, end_id, test_dataset, model, save_path=None, mode='weighted'):
     print("Start results by instance evaluation")
@@ -93,44 +140,7 @@ def results_by_instance(idx2vocab, start_id, end_id, test_dataset, model, save_p
         results.to_csv(save_path, index=None)
 
 
-def compute_traditional_metrics(mode, row_y_test_zeros, row_y_pred_zeros):
-    return {
-        "acc": accuracy_score(row_y_pred_zeros, row_y_test_zeros),
-        "recall": recall_score(row_y_pred_zeros, row_y_test_zeros, average=mode, zero_division=0),
-        "precision": precision_score(row_y_pred_zeros, row_y_test_zeros, average=mode, zero_division=0),
-        "f1": f1_score(row_y_pred_zeros, row_y_test_zeros, average=mode, zero_division=0),
-    }
 
-
-def compute_sequence_metrics(true_seq, pred_seq):
-
-    true_seq_symbols = "".join([symbol_mapping[idx] for idx in true_seq])
-    pred_seq_symbols = "".join([symbol_mapping[idx] for idx in pred_seq])
-    dict_instance_distances = {
-        "levenshtein": textdistance.levenshtein.normalized_similarity(true_seq_symbols, pred_seq_symbols),
-        "damerau_levenshtein": textdistance.damerau_levenshtein.normalized_similarity(true_seq_symbols, pred_seq_symbols),
-        "local_alignment": textdistance.smith_waterman.normalized_similarity(true_seq_symbols, pred_seq_symbols),
-        "global_alignment": textdistance.needleman_wunsch.normalized_similarity(true_seq_symbols, pred_seq_symbols),
-        "emph_start": textdistance.jaro_winkler.normalized_similarity(true_seq_symbols, pred_seq_symbols),
-        "longest_subsequence": textdistance.lcsseq.normalized_similarity(true_seq_symbols, pred_seq_symbols),
-        "longest_substring": textdistance.lcsstr.normalized_similarity(true_seq_symbols, pred_seq_symbols),
-        "overlap": textdistance.overlap.normalized_similarity(true_seq_symbols, pred_seq_symbols),
-        "entropy": textdistance.entropy_ncd.normalized_similarity(true_seq_symbols, pred_seq_symbols),
-    }
-    return dict_instance_distances
-
-
-def compute_decoding(idx2vocab, row_y_pred, row_y_test, row_x_test):
-    x_convert = [f"{i:03d}" for i in row_x_test]
-    return {
-        "input": " | ".join(["-".join(x_convert[:lim + 1]) for lim in range(len(x_convert))]),
-        "true_encoded": " -> ".join([f"{i:03d}" for i in row_y_test]),
-        "pred_encoded": " -> ".join([f"{i:03d}" for i in row_y_pred]),
-        "true_encoded_with_padding": " -> ".join([f"{i:03d}" for i in row_y_test]),
-        "pred_encoded_with_padding": " -> ".join([f"{i:03d}" for i in row_y_pred]),
-        "true_decoded": " -> ".join([idx2vocab[i] for i in row_y_test]),
-        "pred_decoded": " -> ".join([idx2vocab[i] for i in row_y_pred]),
-    }
 
 def compute_pred_seq(idx2vocab, row_y_pred, row_y_test, row_x_test, last_word_test, last_word_pred):
     x_convert = [f"{i:03d}" for i in row_x_test[:last_word_test]]
