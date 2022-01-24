6bce6a8248b4f5183bcb38a26013ec1f6251c49c
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-01-24 17:28:50 +0100

    Rebalancing fails again

--- src/thesis_predictors/runner/run_full_training_outcome.py
+++ src/thesis_predictors/runner/run_full_training_outcome.py
@@ -5,7 +5,7 @@ from thesis_predictors.helper.constants import EVAL_RESULTS_FOLDER, MODEL_FOLDER
 from thesis_readers.readers.DomesticDeclarationsLogReader import DomesticDeclarationsLogReader
 from ..helper.runner import Runner
 from ..models.direct_data_lstm import FullLSTMModelOneWayExtensive, FullLSTMModelOneWaySimple
-from ..models.lstm import TokenToSequenceLSTM, HybridToSequenceLSTM, SimpleLSTMModelTwoWay
+from ..models.lstm import HybridToClassLSTM, TokenToClassLSTM, VectorToClassLSTM
 from ..models.seq2seq_lstm import SeqToSeqSimpleLSTMModelOneWay
 from ..models.transformer import Seq2SeqTransformerModelOneWay, TransformerModelOneWaySimple, TransformerModelTwoWay
 from thesis_readers.helper.modes import FeatureModes, TaskModes
@@ -31,84 +31,47 @@ if __name__ == "__main__":
     evaluator = Evaluator(reader)
 
 
+    reader = DomesticDeclarationsLogReader(debug=False, mode=task_mode)
+    data = reader.init_log(save=True)
+    reader = reader.init_data()
+    evaluator = Evaluator(reader)
+    # adam_init = 0.1
+
     r1 = Runner(
+        TokenToClassLSTM,
         reader,
-        FullLSTMModelOneWaySimple(reader.vocab_len, reader.max_len, reader.feature_len - 1),
         epochs,
         batch_size,
         adam_init,
         num_train=num_train,
         num_val=num_val,
         num_test=num_test,
-        ft_mode=FeatureModes.FULL_SEP, # Make it part of the model
+        ft_mode=FeatureModes.EVENT_ONLY,
     ).train_model().evaluate(evaluator, results_folder, prefix)
     r1.save_model(build_folder, prefix)
-    r3 = Runner(
+
+    r1 = Runner(
+        VectorToClassLSTM,
         reader,
-        HybridToSequenceLSTM(reader.vocab_len, reader.max_len, reader.feature_len),
         epochs,
         batch_size,
         adam_init,
         num_train=num_train,
         num_val=num_val,
         num_test=num_test,
-        ft_mode=FeatureModes.EVENT_ONLY,
+        ft_mode=FeatureModes.FULL,
     ).train_model().evaluate(evaluator, results_folder, prefix)
-    r3.save_model(build_folder, prefix)
-    r5 = Runner(
+    r1.save_model(build_folder, prefix)
+
+    r1 = Runner(
+        HybridToClassLSTM,
         reader,
-        TransformerModelOneWaySimple(reader.vocab_len, reader.max_len),
         epochs,
         batch_size,
         adam_init,
         num_train=num_train,
         num_val=num_val,
         num_test=num_test,
-        ft_mode=FeatureModes.EVENT_ONLY,
+        ft_mode=FeatureModes.FULL_SEP,
     ).train_model().evaluate(evaluator, results_folder, prefix)
-    r5.save_model(build_folder, prefix)
-    
-    
-    # Setup Reader and Evaluator ! OVERFITS
-    # task_mode = TaskModes.OUTCOME_EXTENSIVE
-    # reader = DomesticDeclarationsLogReader(debug=False, mode=task_mode)
-    # data = reader.init_log(save=True)
-    # reader = reader.init_data()
-    # evaluator = Evaluator(reader)
-    
-    # r1 = Runner(
-    #     reader,
-    #     FullLSTMModelOneWayExtensive(reader.vocab_len, reader.max_len, reader.feature_len - 1),
-    #     epochs,
-    #     batch_size,
-    #     adam_init,
-    #     num_train=num_train,
-    #     num_val=num_val,
-    #     num_test=num_test,
-    #     ft_mode=FeatureModes.FULL_SEP, # Make it part of the model
-    # ).train_model().evaluate(evaluator, results_folder, prefix)
-    # r1.save_model(build_folder, prefix)
-    # r3 = Runner(
-    #     reader,
-    #     SimpleLSTMModelOneWayExtensive(reader.vocab_len, reader.max_len, reader.feature_len),
-    #     epochs,
-    #     batch_size,
-    #     adam_init,
-    #     num_train=num_train,
-    #     num_val=num_val,
-    #     num_test=num_test,
-    #     ft_mode=FeatureModes.EVENT_ONLY,
-    # ).train_model().evaluate(evaluator, results_folder, prefix)
-    # r3.save_model(build_folder, prefix)
-    # r5 = Runner(
-    #     reader,
-    #     TransformerModelOneWayExtensive(reader.vocab_len, reader.max_len),
-    #     epochs,
-    #     batch_size,
-    #     adam_init,
-    #     num_train=num_train,
-    #     num_val=num_val,
-    #     num_test=num_test,
-    #     ft_mode=FeatureModes.EVENT_ONLY,
-    # ).train_model().evaluate(evaluator, results_folder, prefix)
-    # r5.save_model(build_folder, prefix)
+    r1.save_model(build_folder, prefix)
