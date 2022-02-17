82cc27430230bbfeb7d43832d2b7bbbdcb7101d4
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-02-18 00:49:47 +0100

    Impl: Stub of callbacks

---
+++ src/thesis_commons/callbacks.py
@@ -0,0 +1,27 @@
+import pathlib
+from typing import List
+import tensorflow as tf
+import tensorflow.keras as keras
+import tensorflow.keras.backend as K
+from tensorflow.keras import layers
+import numpy as np
+from tensorflow.keras import losses
+from tensorflow.keras import callbacks
+
+
+class CallbackCollection:
+
+    def __init__(self, model_path: pathlib.Path, is_prod:bool=False, ) -> None:
+        self.model_path = model_path
+        self.tboard_path = pathlib.Path('/')
+        self.cb_list = []
+
+    def add(self, cb:callbacks.Callback):
+        self.cb_list.append(cb)
+        return self
+
+    def build(self):
+        self.cb_list.append(callbacks.CSVLogger(filename=self.model_path / "history.csv"))
+        self.cb_list.append(callbacks.TensorBoard(log_dir='logs'))
+        self.cb_list.append(callbacks.ModelCheckpoint(filepath=self.model_path))
+        return self.cb_list
