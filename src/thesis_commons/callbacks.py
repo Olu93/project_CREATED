c4011c23617bf645a42b582c29f363b5e2426a3a
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-02-18 00:52:17 +0100

    Minor change

--- src/thesis_commons/callbacks.py
+++ src/thesis_commons/callbacks.py
@@ -11,17 +11,22 @@ from tensorflow.keras import callbacks
 
 class CallbackCollection:
 
-    def __init__(self, model_path: pathlib.Path, is_prod:bool=False, ) -> None:
+    def __init__(
+        self,
+        model_path: pathlib.Path,
+        is_prod: bool = False,
+    ) -> None:
         self.model_path = model_path
         self.tboard_path = pathlib.Path('/')
         self.cb_list = []
+        self.is_prod = is_prod
 
-    def add(self, cb:callbacks.Callback):
+    def add(self, cb: callbacks.Callback):
         self.cb_list.append(cb)
         return self
 
     def build(self):
         self.cb_list.append(callbacks.CSVLogger(filename=self.model_path / "history.csv"))
         self.cb_list.append(callbacks.TensorBoard(log_dir='logs'))
-        self.cb_list.append(callbacks.ModelCheckpoint(filepath=self.model_path))
+        self.cb_list.append(callbacks.ModelCheckpoint(filepath=self.model_path, verbose=0 if self.is_prod else 1, save_best_only=self.is_prod))
         return self.cb_list
