fd340d6434e22692cf728bb582010f8fe4c3ae19
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-02-24 18:31:54 +0100

    Impl: Rewrite of the DMM
    Different output
    Name refactoring

--- src/thesis_generators/models/vae/vae_dmm_stepwise.py
+++ src/thesis_generators/models/vae/vae_dmm_stepwise.py
@@ -36,59 +36,22 @@ class DMMModel(commons.GeneratorPartMixin):
         self.is_initial = True
         self.future_encoder = FutureSeqEncoder(self.ff_dim)
         self.state_transitioner = TransitionModel(self.ff_dim)
-        self.inference_encoder = InferenceEncoderModel(self.ff_dim)
+        self.inferencer = InferenceModel(self.ff_dim)
         self.sampler = Sampler(self.ff_dim)
-        self.inference_decoder = InferenceDecoderModel(self.feature_len)
-        self.reconstuctor = ReconstructionModel(self.feature_len)
+        self.emitter = EmissionModel(self.feature_len)
 
-    # def train_step(self, data):
-    #     (events, features), _ = data
-    #     metrics_collector = {}
-    #     # Train Process
-    #     decomposed_losses = []
-    #     loss = 0
-    #     kl_part_loss = 0
-    #     rec_part_loss = 0
-    #     with tf.GradientTape() as tape:
-    #         embeddings = self.embedder([events, features])
-    #         gt_backwards = self.future_encoder(embeddings)
-    #         zt_sample = tf.keras.backend.random_uniform(shape=gt_backwards.shape)[:, 0]
-    #         # zt_sample = tf.repeat(self.initial_z, len(gt_backwards), axis=0)
-    #         for t in range(embeddings.shape[1]):
-    #             zt_prev = zt_sample
-    #             xt = embeddings[:, t]
-    #             gt = gt_backwards[:, t]
 
-    #             z_transition_mu, z_transition_logvar = self.state_transitioner(zt_prev)
-    #             z_inference_mu, z_inference_logvar = self.inference_encoder([gt, zt_prev])
-
-    #             zt_sample = self.sampler([z_inference_mu, z_inference_logvar])
-    #             zt_mean, zt_logvar = self.inference_decoder(zt_sample)
-    #             xt_sample = self.reconstuctor([zt_mean, zt_logvar])
-    #             seq_vae_loss = self.loss(xt, [xt_sample, [z_transition_mu, z_transition_logvar], [z_inference_mu, z_inference_logvar]])
-
-    #             loss += seq_vae_loss
-    #             decomposed_losses.append(dict(self.loss.loss_decomposed))
-
-    #     all_models = [self.embedder, self.future_encoder, self.state_transitioner, self.inference_encoder, self.inference_decoder, self.reconstuctor]
-    #     trainable_weights = [w for md in all_models for w in md.trainable_weights]
-    #     grads = tape.gradient(loss, trainable_weights)
-    #     self.optimizer.apply_gradients(zip(grads, trainable_weights))
-    #     metrics_collector["loss"] = loss
-    #     metrics_collector["kl_loss"] = kl_part_loss
-    #     metrics_collector["rec_loss"] = rec_part_loss
-    #     return metrics_collector
-
-    def compile(self, optimizer='adam', loss=None, metrics=None, loss_weights=None, weighted_metrics=None, run_eagerly=None, steps_per_execution=None, **kwargs):
+    def compile(self, optimizer=None, loss=None, metrics=None, loss_weights=None, weighted_metrics=None, run_eagerly=None, steps_per_execution=None, **kwargs):
         loss = metric.SeqELBOLoss()
         return super().compile(optimizer, loss, metrics, loss_weights, weighted_metrics, run_eagerly, steps_per_execution, **kwargs)
 
     def call(self, inputs, training=None, mask=None):
-        sampled_x_list = []
         sampled_z_tra_mean_list = []
         sampled_z_tra_logvar_list = []
         sampled_z_inf_mean_list = []
         sampled_z_inf_logvar_list = []
+        sampled_x_emi_mean_list = []
+        sampled_x_emi_logvar_list = []
         gt_backwards = self.future_encoder(inputs)
 
         zt_sample = tf.keras.backend.zeros_like(gt_backwards)[:, 0]
@@ -99,23 +62,25 @@ class DMMModel(commons.GeneratorPartMixin):
             gt = gt_backwards[:, t]
 
             z_transition_mu, z_transition_logvar = self.state_transitioner(zt_prev)
-            z_inference_mu, z_inference_logvar = self.inference_encoder([gt, zt_prev])
+            z_inf_mu, z_inf_logvar = self.inferencer([gt, zt_prev])
 
-            zt_sample = self.sampler([z_inference_mu, z_inference_logvar])
-            zt_mean, zt_logvar = self.inference_decoder(zt_sample)
-            xt_sample = self.reconstuctor([zt_mean, zt_logvar])
-            # seq_vae_loss = self.loss(xt, [xt_sample, [z_transition_mu, z_transition_logvar], [z_inference_mu, z_inference_logvar]])
-            sampled_x_list.append(xt_sample)
+            zt_sample = self.sampler([z_inf_mu, z_inf_logvar])
+            xt_emi_mu, xt_emi_logvar = self.emitter(zt_sample)
+            
             sampled_z_tra_mean_list.append(z_transition_mu)
             sampled_z_tra_logvar_list.append(z_transition_logvar)
-            sampled_z_inf_mean_list.append(z_inference_mu)
-            sampled_z_inf_logvar_list.append(z_inference_logvar)
-        sampled_x = tf.stack(sampled_x_list, axis=1)
+            sampled_z_inf_mean_list.append(z_inf_mu)
+            sampled_z_inf_logvar_list.append(z_inf_logvar)
+            sampled_x_emi_mean_list.append(xt_emi_mu)
+            sampled_x_emi_logvar_list.append(xt_emi_logvar)
+            
         sampled_z_tra_mean = tf.stack(sampled_z_tra_mean_list, axis=1)
         sampled_z_tra_logvar = tf.stack(sampled_z_tra_logvar_list, axis=1)
         sampled_z_inf_mean = tf.stack(sampled_z_inf_mean_list, axis=1)
         sampled_z_inf_logvar = tf.stack(sampled_z_inf_logvar_list, axis=1)
-        return sampled_x, [sampled_z_tra_mean, sampled_z_tra_logvar], [sampled_z_inf_mean, sampled_z_inf_logvar]
+        sampled_x_emi_mean = tf.stack(sampled_x_emi_mean_list, axis=-1)
+        sampled_x_emi_logvar = tf.stack(sampled_x_emi_logvar_list, axis=-1)
+        return [sampled_x_emi_mean, sampled_x_emi_logvar], [sampled_z_tra_mean, sampled_z_tra_logvar], [sampled_z_inf_mean, sampled_z_inf_logvar]
 
 
 class DMMnterpretorModel(commons.InterpretorPartMixin):
@@ -162,8 +127,8 @@ class TransitionModel(Model):
 
     def __init__(self, ff_dim):
         super(TransitionModel, self).__init__()
-        self.latent_vector_z_mean = layers.Dense(ff_dim, name="z_mean")
-        self.latent_vector_z_log_var = layers.Dense(ff_dim, name="z_log_var")
+        self.latent_vector_z_mean = layers.Dense(ff_dim, name="z_tra_mean")
+        self.latent_vector_z_log_var = layers.Dense(ff_dim, name="z_tra_logvar")
 
     def call(self, inputs, training=None, mask=None):
         z_t_minus_1 = inputs
@@ -173,13 +138,13 @@ class TransitionModel(Model):
 
 
 # https://youtu.be/rz76gYgxySo?t=1483
-class InferenceEncoderModel(Model):
+class InferenceModel(Model):
 
     def __init__(self, ff_dim):
-        super(InferenceEncoderModel, self).__init__()
+        super(InferenceModel, self).__init__()
         self.combiner = layers.Concatenate()
-        self.latent_vector_z_mean = layers.Dense(ff_dim, name="z_mean")
-        self.latent_vector_z_log_var = layers.Dense(ff_dim, name="z_log_var")
+        self.latent_vector_z_mean = layers.Dense(ff_dim, name="z_inf_mean")
+        self.latent_vector_z_log_var = layers.Dense(ff_dim, name="z_inf_logvar")
 
     def call(self, inputs, training=None, mask=None):
         g_t_backwards, z_t_minus_1 = inputs
@@ -201,12 +166,12 @@ class Sampler(layers.Layer):
         return z_mean + tf.exp(0.5 * z_log_var) * epsilon
 
 
-class InferenceDecoderModel(Model):
+class EmissionModel(Model):
 
     def __init__(self, feature_len):
-        super(InferenceDecoderModel, self).__init__()
-        self.latent_vector_z_mean = layers.Dense(feature_len, name="z_mean")
-        self.latent_vector_z_log_var = layers.Dense(feature_len, name="z_log_var")
+        super(EmissionModel, self).__init__()
+        self.latent_vector_z_mean = layers.Dense(feature_len, name="z_emi_mean")
+        self.latent_vector_z_log_var = layers.Dense(feature_len, name="z_emi_logvar")
 
     def call(self, inputs):
         z_sample = inputs
@@ -216,13 +181,3 @@ class InferenceDecoderModel(Model):
         return z_mean, z_log_var
 
 
-class ReconstructionModel(Model):
-
-    def __init__(self, feature_len):
-        super(ReconstructionModel, self).__init__()
-        self.sampler = Sampler(feature_len)
-
-    def call(self, inputs):
-        z_mean, z_log_var = inputs
-        x_reconstructed = self.sampler([z_mean, z_log_var])
-        return x_reconstructed
