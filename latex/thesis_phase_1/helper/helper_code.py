d5e438a8ba1b8ec4c33310c3ae335aa0c6a64857
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-03-14 02:07:28 +0100

    Paper: rewritten mlts part

--- latex/thesis_phase_1/helper/helper_code.py
+++ latex/thesis_phase_1/helper/helper_code.py
@@ -1,16 +1,19 @@
 import itertools as it
 
-
 set_objects_2 = 'AB'
 set_objects_3 = 'ABC'
 set_objects_4 = 'ABCD'
 
-x = set(
-    list(it.chain(*[list(it.permutations(obj)) for obj in list(it.chain(*[list(it.combinations_with_replacement(set_objects_3, i)) for i in range(0,
-                                                                                                                                                  len(set_objects_3) + 1)]))])))
+
+def generate_production_set(set_objects):
+    return set(
+    list(it.chain(*[list(it.permutations(obj)) for obj in list(it.chain(*[list(it.combinations_with_replacement(set_objects, i)) for i in range(0,
+                                                                                                                                                  len(set_objects) + 1)]))])))
 
 
-def check_if_c_after_d(s):
+x = generate_production_set(set_objects_3)
+
+def check_if_c_before_d(s):
     if 'D' not in s:
         return True
     for i in reversed(range(len(s))):
@@ -19,11 +22,20 @@ def check_if_c_after_d(s):
                 return False
     return True
 
+def check_if_d_after_c(s):
+    if 'C' not in s:
+        return True
+    for i in range(len(s)):
+        if s[i] == 'C':
+            if i == len(s)-1:
+                return False
+            if s[i + 1] != 'D':
+                return False
+    return True
 
+without_restrict = generate_production_set(set_objects_4)
 with_restrict = [
-    s for s in set(
-        list(
-            it.chain(*[list(it.permutations(obj)) for obj in list(it.chain(*[list(it.combinations_with_replacement(set_objects_4, i)) for i in range(0,
-                                                                                                                                                     len(set_objects_4) + 1)]))])))
-    if check_if_c_after_d(s)
+    s for s in generate_production_set(set_objects_4)
+    if check_if_d_after_c(s)
 ]
+with_restrict
