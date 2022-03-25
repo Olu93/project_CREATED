447a78de6710e60a3ab6839bc16e47540306c574
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-03-25 02:00:56 +0100

    Fix: stepwise version runs but doesn't work

--- src/thesis_generators/models/vae/vae_dmm_stepwise.py
+++ src/thesis_generators/models/vae/vae_dmm_stepwise.py
@@ -30,8 +30,9 @@ class DMMModelStepwise(commons.GeneratorPartMixin):
         self.state_transitioner = TransitionModel(self.ff_dim)
         self.inferencer = InferenceModel(self.ff_dim)
         self.sampler = commons.Sampler()
-        self.emitter_events = EmissionModel(self.vocab_len)
-        self.emitter_features = EmissionModel(self.feature_len)
+        self.emitter_events = EmissionEvModel(self.vocab_len)
+        self.emitter_features = EmissionFtModel(self.feature_len)
+        self.combiner = layers.Concatenate()
         self.masker = layers.Masking()
 
 
@@ -46,32 +47,29 @@ class DMMModelStepwise(commons.GeneratorPartMixin):
         sampled_z_tra_logvar_list = []
         sampled_z_inf_mean_list = []
         sampled_z_inf_logvar_list = []
-        sampled_x_emi_mean_list_events = []
-        sampled_x_emi_logvar_list_events = []
+        sampled_x_probs_list_events = []
         sampled_x_emi_mean_list_features = []
         sampled_x_emi_logvar_list_features = []
-        gt_backwards = self.future_encoder(inputs)
+        x = self.future_encoder(inputs)
 
-        zt_sample = tf.keras.backend.zeros_like(gt_backwards)[:, 0]
+        zt_sample = tf.keras.backend.zeros_like(x)[:, 0]
         # zt_sample = tf.repeat(self.initial_z, len(gt_backwards), axis=0)
         for t in range(inputs.shape[1]):
             zt_prev = zt_sample
-            xt = inputs[:, t]
-            gt = gt_backwards[:, t]
+            xt = x[:, t]
 
             z_transition_mu, z_transition_logvar = self.state_transitioner(zt_prev)
-            z_inf_mu, z_inf_logvar = self.inferencer([gt, zt_prev])
+            z_inf_mu, z_inf_logvar = self.inferencer(self.combiner([xt, zt_prev]))
 
             zt_sample = self.sampler([z_inf_mu, z_inf_logvar])
-            xt_emi_mu_events, xt_emi_logvar_events = self.emitter_events(zt_sample)
+            xt_emi_ev_probs = self.emitter_events(zt_sample)
             xt_emi_mu_features, xt_emi_logvar_features = self.emitter_features(zt_sample)
             
             sampled_z_tra_mean_list.append(z_transition_mu)
             sampled_z_tra_logvar_list.append(z_transition_logvar)
             sampled_z_inf_mean_list.append(z_inf_mu)
             sampled_z_inf_logvar_list.append(z_inf_logvar)
-            sampled_x_emi_mean_list_events.append(xt_emi_mu_events)
-            sampled_x_emi_logvar_list_events.append(xt_emi_logvar_events)
+            sampled_x_probs_list_events.append(xt_emi_ev_probs)
             sampled_x_emi_mean_list_features.append(xt_emi_mu_features)
             sampled_x_emi_logvar_list_features.append(xt_emi_logvar_features)
             
@@ -79,41 +77,16 @@ class DMMModelStepwise(commons.GeneratorPartMixin):
         sampled_z_tra_logvar = tf.stack(sampled_z_tra_logvar_list, axis=1)
         sampled_z_inf_mean = tf.stack(sampled_z_inf_mean_list, axis=1)
         sampled_z_inf_logvar = tf.stack(sampled_z_inf_logvar_list, axis=1)
-        sampled_x_emi_mean_events = tf.stack(sampled_x_emi_mean_list_events, axis=1)
-        sampled_x_emi_logvar_events = tf.stack(sampled_x_emi_logvar_list_events, axis=1)
+        sampled_x_emi_mean_events = tf.stack(sampled_x_probs_list_events, axis=1)
         sampled_x_emi_mean_features = tf.stack(sampled_x_emi_mean_list_features, axis=1)
         sampled_x_emi_logvar_features = tf.stack(sampled_x_emi_logvar_list_features, axis=1)
 
         r_tra_params = tf.stack([sampled_z_tra_mean, sampled_z_tra_logvar], axis=-2)
         r_inf_params = tf.stack([sampled_z_inf_mean, sampled_z_inf_logvar], axis=-2)
-        r_emi_ev_params = tf.stack([sampled_x_emi_mean_events, sampled_x_emi_logvar_events], axis=-2)
+        r_emi_ev_params = sampled_x_emi_mean_events
         r_emi_ft_params = tf.stack([sampled_x_emi_mean_features, sampled_x_emi_logvar_features], axis=-2)
 
         return r_tra_params, r_inf_params, r_emi_ev_params, r_emi_ft_params
-
-
-class DMMnterpretorModel(commons.InterpretorPartMixin):
-
-    def __init__(self, *args, **kwargs):
-        super(DMMnterpretorModel, self).__init__(*args, **kwargs)
-        # Either trainined in conjunction to generator or seperately
-        self.ff_dim = kwargs.get('ff_dim')
-        self.vocab_len = kwargs.get('vocab_len')
-        self.lstm_layer = layers.Bidirectional(layers.LSTM(self.ff_dim, return_sequences=True))
-        self.output_layer = layers.TimeDistributed(layers.Dense(self.vocab_len))
-        self.activation_layer = layers.Softmax()
-
-    def compile(self, optimizer=None, loss=None, metrics=None, loss_weights=None, weighted_metrics=None, run_eagerly=None, steps_per_execution=None, **kwargs):
-        loss = metric.JoinedLoss([metric.MSpCatCE(name="cat_ce")])
-        # metrics = []
-        return super().compile(optimizer, loss, metrics, loss_weights, weighted_metrics, run_eagerly, steps_per_execution, **kwargs)
-
-    def call(self, inputs, training=None, mask=None):
-        pred_event_probs = inputs
-        x = self.lstm_layer(pred_event_probs)
-        x = self.output_layer(x)
-        x = self.activation_layer(x)
-        return x
 
 
 # https://youtu.be/rz76gYgxySo?t=1383
@@ -136,13 +109,14 @@ class TransitionModel(Model):
 
     def __init__(self, ff_dim):
         super(TransitionModel, self).__init__()
-        self.latent_vector_z_mean = layers.Dense(ff_dim, name="z_tra_mean", activation='tanh')
-        self.latent_vector_z_log_var = layers.Dense(ff_dim, name="z_tra_logvar", activation='tanh')
+        self.hidden = layers.Dense(ff_dim, name="z_tra_hidden", activation='relu')
+        self.latent_vector_z_mean = layers.Dense(ff_dim, name="z_tra_mean", activation='linear')
+        self.latent_vector_z_log_var = layers.Dense(ff_dim, name="z_tra_logvar", activation='softplus')
 
     def call(self, inputs, training=None, mask=None):
-        z_t_minus_1 = inputs
-        z_mean = self.latent_vector_z_mean(z_t_minus_1)
-        z_log_var = self.latent_vector_z_log_var(z_t_minus_1)
+        x = self.hidden(inputs)
+        z_mean = self.latent_vector_z_mean(x)
+        z_log_var = self.latent_vector_z_log_var(x)
         return z_mean, z_log_var
 
 
@@ -152,42 +126,40 @@ class InferenceModel(Model):
     def __init__(self, ff_dim):
         super(InferenceModel, self).__init__()
         self.combiner = layers.Concatenate()
-        self.latent_vector_z_mean = layers.Dense(ff_dim, name="z_inf_mean", activation='tanh')
-        self.latent_vector_z_log_var = layers.Dense(ff_dim, name="z_inf_logvar", activation='tanh')
+        self.hidden = layers.Dense(ff_dim, name="z_inf_hidden", activation='relu')
+        self.latent_vector_z_mean = layers.Dense(ff_dim, name="z_inf_mean", activation='linear')
+        self.latent_vector_z_log_var = layers.Dense(ff_dim, name="z_inf_logvar", activation='softplus')
 
     def call(self, inputs, training=None, mask=None):
-        g_t_backwards, z_t_minus_1 = inputs
-        combined_input = self.combiner([g_t_backwards, z_t_minus_1])
-        z_mean = self.latent_vector_z_mean(combined_input)
-        z_log_var = self.latent_vector_z_log_var(combined_input)
+        x = self.hidden(inputs)
+        z_mean = self.latent_vector_z_mean(x)
+        z_log_var = self.latent_vector_z_log_var(x)
         return z_mean, z_log_var
 
 
-class Sampler(layers.Layer):
-    # TODO: centralise this layer for broad use to reduce code repetition
-    """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
-
-    def call(self, inputs):
-        z_mean, z_log_var = inputs
-        batch = tf.shape(z_mean)[0]
-        dim = tf.shape(z_mean)[1]
-        epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
-        # Explained here https://jaketae.github.io/study/vae/
-        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
-
-
-class EmissionModel(Model):
+class EmissionFtModel(Model):
 
     def __init__(self, feature_len):
-        super(EmissionModel, self).__init__()
-        self.latent_vector_z_mean = layers.Dense(feature_len, name="z_emi_mean", activation='tanh')
-        self.latent_vector_z_log_var = layers.Dense(feature_len, name="z_emi_logvar", activation='tanh')
+        super(EmissionFtModel, self).__init__()
+        self.hidden = layers.Dense(feature_len, name="x_ft_hidden", activation='relu')
+        self.latent_vector_z_mean = layers.Dense(feature_len, name="x_ft_mean", activation='linear')
+        self.latent_vector_z_log_var = layers.Dense(feature_len, name="x_ft_logvar", activation='softplus')
 
     def call(self, inputs):
-        z_sample = inputs
-
+        z_sample = self.hidden(inputs)
         z_mean = self.latent_vector_z_mean(z_sample)
         z_log_var = self.latent_vector_z_log_var(z_sample)
         return z_mean, z_log_var
 
 
+class EmissionEvModel(Model):
+
+    def __init__(self, feature_len):
+        super(EmissionEvModel, self).__init__()
+        self.hidden = layers.Dense(feature_len, name="x_ev_hidden", activation='relu')
+        self.latent_vector_z_mean = layers.Dense(feature_len, name="x_ev", activation='softmax')
+
+    def call(self, inputs):
+        z_sample = self.hidden(inputs)
+        z_mean = self.latent_vector_z_mean(z_sample)
+        return z_mean
