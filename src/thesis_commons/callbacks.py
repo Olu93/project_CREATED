6497a3ccc05927651935a50c155ac5a95f8805db
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-04-11 21:54:29 +0200

    Fix: Issue with saving directories

--- src/thesis_commons/callbacks.py
+++ src/thesis_commons/callbacks.py
@@ -17,13 +17,13 @@ class CallbackCollection:
     def __init__(
         self,
         model_name: str,
-        model_path: pathlib.Path,
+        models_dir: pathlib.Path,
         is_prod: bool = False,
     ) -> None:
         self.model_name = model_name
-        self.model_path = model_path
-        tmp_chkpt_path = create_path("chkpt_path", self.model_path / self.model_name)
-        self.chkpt_path = tmp_chkpt_path / self.model_name
+        self.models_dir = models_dir
+        tmp_chkpt_path = create_path("chkpt_path", self.models_dir / self.model_name)
+        self.chkpt_path = tmp_chkpt_path / "checkpoint"
         self.tboard_path = create_path("tboard_path", PATH_ROOT / 'logs' / self.model_name)
         self.csv_logger_path = tmp_chkpt_path / "history.csv"
         self.cb_list = []
