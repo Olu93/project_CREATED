aeb52c4b7dd282c28bef8fae4e6bb880cb605f28
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-04-28 23:24:30 +0200

    Fix: I think Ifixed issues by downgrading to tf 2.7

--- src/thesis_commons/libcuts.py
+++ src/thesis_commons/libcuts.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
-import keras.api._v2.keras as keras
-from keras.api._v2.keras import backend, optimizers, layers, models, losses, metrics, utils
-K = backend
-# from tensorflow import keras
-# from tensorflow.keras import  optimizers, layers, models, losses, metrics, utils, backend as K
+# import keras.api._v2.keras as keras
+# from keras.api._v2.keras import backend, optimizers, layers, models, losses, metrics, utils
+# K = backend
+from tensorflow import keras
+from tensorflow.keras import  optimizers, layers, models, losses, metrics, utils, backend as K
