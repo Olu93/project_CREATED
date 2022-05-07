76b377c8240920f479e1ec7ba13ff16675f008c5
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-05-07 05:03:52 +0200

    Ref: Deleted some runners

--- src/thesis_predictors/runner/run_full_training_outcome.py
+++ src/thesis_predictors/runner/run_full_training_outcome.py
@@ -2,69 +2,38 @@ import tensorflow as tf
 from thesis_predictors.helper.evaluation import Evaluator
 from thesis_predictors.helper.constants import EVAL_RESULTS_FOLDER, MODEL_FOLDER
 
-from thesis_readers.readers.DomesticDeclarationsLogReader import DomesticDeclarationsLogReader
+# from thesis_readers.readers.DomesticDeclarationsLogReader import DomesticDeclarationsLogReader as Reader
+# from thesis_readers import RequestForPaymentLogReader as Reader
+from thesis_readers.readers.MockReader import MockReader as Reader
 from ..helper.runner import Runner
-from ..models.direct_data_lstm import FullLSTMModelOneWayExtensive, FullLSTMModelOneWaySimple
-from ..models.lstms.lstm import HybridToClassLSTM, TokenToClassLSTM, VectorToClassLSTM
-from ..models.seq2seq_lstm import SeqToSeqSimpleLSTMModelOneWay
-from ..models.transformer import Seq2SeqTransformerModelOneWay, TransformerModelOneWaySimple, TransformerModelTwoWay
+from ..models.lstms.lstm import EmbeddingLSTM as PredictionModel
+# from ..models.lstms.lstm import SimpleLSTM as PredictionModel 
+# from ..models.lstms.lstm import BaseLSTM as PredictionModel
 from thesis_commons.modes import FeatureModes, TaskModes
-from thesis_readers import RequestForPaymentLogReader
 
+DEBUG = False
 if __name__ == "__main__":
     # Parameters
     results_folder = EVAL_RESULTS_FOLDER
     build_folder = MODEL_FOLDER
-    prefix = "result_outcome"
-    epochs = 10
-    batch_size = 64
-    adam_init = 0.001
+    prefix = "result_next"
+    epochs = 50 if not DEBUG else 2
+    batch_size = 8 if not DEBUG else 64
+    adam_init = 0.1
     num_train = None
     num_val = None
-    num_test = None    
+    num_test = None
 
     # Setup Reader and Evaluator
-    task_mode = TaskModes.OUTCOME
-    reader = DomesticDeclarationsLogReader(debug=False, mode=task_mode)
-    data = reader.init_log(save=True)
-    reader = reader.init_meta()
-    evaluator = Evaluator(reader)
-
-
-    reader = DomesticDeclarationsLogReader(debug=False, mode=task_mode)
+    task_mode = TaskModes.NEXT_EVENT_EXTENSIVE
+    reader = Reader(debug=False, mode=task_mode)
     data = reader.init_log(save=True)
     reader = reader.init_meta()
     evaluator = Evaluator(reader)
     # adam_init = 0.1
 
     r1 = Runner(
-        TokenToClassLSTM,
-        reader,
-        epochs,
-        batch_size,
-        adam_init,
-        num_train=num_train,
-        num_val=num_val,
-        num_test=num_test,
-        ft_mode=FeatureModes.EVENT_ONLY,
-    ).train_model().evaluate(evaluator, results_folder, prefix)
-    r1.save_model(build_folder, prefix)
-
-    r1 = Runner(
-        VectorToClassLSTM,
-        reader,
-        epochs,
-        batch_size,
-        adam_init,
-        num_train=num_train,
-        num_val=num_val,
-        num_test=num_test,
-        ft_mode=FeatureModes.FULL,
-    ).train_model().evaluate(evaluator, results_folder, prefix)
-    r1.save_model(build_folder, prefix)
-
-    r1 = Runner(
-        HybridToClassLSTM,
+        PredictionModel,
         reader,
         epochs,
         batch_size,
@@ -74,4 +43,6 @@ if __name__ == "__main__":
         num_test=num_test,
         ft_mode=FeatureModes.FULL_SEP,
     ).train_model().evaluate(evaluator, results_folder, prefix)
-    r1.save_model(build_folder, prefix)
+    # r1.save_model(build_folder, prefix)
+    print("done")
+    
---
+++ src/thesis_readers/readers/OutcomeReader.py
@@ -0,0 +1,50 @@
+from thesis_readers.helper.helper import test_reader
+from thesis_readers.helper.constants import DATA_FOLDER_PREPROCESSED, DATA_FOLDER
+from .AbstractProcessLogReader import AbstractProcessLogReader, CSVLogReader
+import pandas as pd
+from pm4py.objects.log.util import dataframe_utils
+from pm4py.objects.conversion.log import converter as log_converter
+import pm4py
+import category_encoders as ce
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
+TO_EVENT_LOG = log_converter.Variants.TO_EVENT_LOG
+
+class OutcomeBPIC2011(CSVLogReader):
+    COL_LIFECYCLE = "lifecycle:transition"
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(
+            log_path= DATA_FOLDER/ 'dataset_various_outcome_prediction/BPIC11_f1.csv',
+            csv_path= DATA_FOLDER_PREPROCESSED/ 'RabobankItil.csv',
+            sep=";",
+            col_case_id="Incident ID",
+            col_event_id="IncidentActivity_Type",
+            col_timestamp="DateStamp",
+            **kwargs,
+        )
+
+    def preprocess_level_general(self):
+        super().preprocess_level_general(remove_cols=None)
+
+    def preprocess_level_specialized(self, **kwargs):
+        cat_encoder = ce.BaseNEncoder(verbose=1, return_df=True, base=2)
+        num_encoder = StandardScaler()
+
+        categorical_columns = list(self.data.select_dtypes('object').columns.drop([self.col_activity_id, self.col_case_id]))
+        # normalization_columns = list(self.data.select_dtypes('number').columns)
+        self.data = self.data.join(cat_encoder.fit_transform(self.data[categorical_columns]))
+
+        # self.data[normalization_columns] = num_encoder.fit_transform(self.data[normalization_columns])
+        self.data = self.data.drop(categorical_columns, axis=1)
+
+        self.preprocessors['categoricals'] = cat_encoder
+        self.preprocessors['normalized'] = num_encoder
+        super().preprocess_level_specialized(**kwargs)
+
+    
+    
+if __name__ == '__main__':
+    reader = OutcomeBPIC2011(debug=True)
+    test_reader(reader, True)
+
