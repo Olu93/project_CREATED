98405651f80fe82cd9961ced6010aa1a8fa410c7
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-05-12 02:58:45 +0200

    Impl: Some changes for intellisense

--- src/thesis_commons/libcuts.py
+++ src/thesis_commons/libcuts.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 # import keras.api._v2.keras as keras
-# from keras.api._v2.keras import backend, optimizers, layers, models, losses, metrics, utils
 # K = backend
 from tensorflow import keras
-from tensorflow.keras import  optimizers, layers, models, losses, metrics, utils, backend as K
+# from tensorflow.keras import  optimizers, layers, models, losses, metrics, utils
+from tensorflow.python.keras import backend as K
+from keras.api._v2.keras import optimizers, layers, models, losses as ls, metrics, utils
+
+losses = ls
