009810dfa69d853dc3d95fbb7c07e7afd304be94
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-05-21 18:30:00 +0200

    Fixes and run example for evolutionary generator

--- src/thesis_generators/models/evolutionary_strategies/skeleton.py
+++ src/thesis_generators/models/evolutionary_strategies/skeleton.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 from thesis_generators.models.model_commons import BaseModelMixin
 
 from thesis_viability.viability.viability_function import ViabilityMeasure
@@ -8,9 +8,7 @@ from thesis_viability.viability.viability_function import ViabilityMeasure
 DEBUG_STOP = 1000
 
 
-class EvolutionaryStrategy(BaseModelMixin, ABCMeta):
-    evolutionary_counter: int = None
-    max_iter: int = None
+class EvolutionaryStrategy(BaseModelMixin, ABC):
 
     def __init__(self, evaluator: ViabilityMeasure, max_iter: int = 1000, survival_thresh: int = 5, num_population: int = 100, **kwargs) -> None:
         super().__init__(**kwargs)
@@ -19,44 +17,45 @@ class EvolutionaryStrategy(BaseModelMixin, ABCMeta):
         self.name = self.__class__.__name__
         self.num_survivors = survival_thresh
         self.num_population = num_population
+        self.evolutionary_counter = 0
 
-    def __call__(self, factual_seeds):
+    def __call__(self, factual_seeds, labels):
         all_generated = []
-        for fc_seed in self.__next_seed(factual_seeds):
+        for fc_seed, fc_outcome in self.__next_seed(factual_seeds, labels):
             cf_offspring = self.generate_offspring(None, fc_seed)
-            fitness_values = self.determine_fitness(cf_offspring)
+            fitness_values = self.determine_fitness(cf_offspring, fc_seed)[0]
             cf_survivors = self.pick_survivors(cf_offspring, fitness_values)
             cf_parents = cf_survivors
 
             while self.__is_termination(cf_survivors, fitness_values, self.evolutionary_counter, fc_seed):
                 cf_offspring = self.generate_offspring(cf_parents, fc_seed)
-                fitness_values = self.determine_fitness(cf_offspring, fc_seed)
+                fitness_values = self.determine_fitness(cf_offspring, fc_seed)[0]
                 cf_survivors = self.pick_survivors(cf_offspring, fitness_values)
                 cf_parents = cf_survivors
                 self.__add_counter()
 
             final_population = cf_parents
-            final_fitness = self.determine_fitness(final_population)
+            final_fitness = self.determine_fitness(final_population, fc_seed)
             all_generated.append((final_population, final_fitness))
 
         return all_generated
 
-    def generate_offspring(self, cf_parents, factual_seed, **kwargs):
+    def generate_offspring(self, cf_parents, fc_seed, **kwargs):
         if cf_parents is None:
-            offspring = self._init_population(factual_seed)
+            offspring = self._init_population(fc_seed)
             return offspring
-        offspring = self._generate_population(cf_parents, factual_seed)
-        mutated = self._mutate_offspring(offspring, factual_seed)
-        return offspring
+        offspring = self._generate_population(cf_parents, fc_seed)
+        mutated = self._mutate_offspring(offspring, fc_seed)
+        return mutated
 
-    def __next_seed(self, factual_seeds):
+    def __next_seed(self, factual_seeds, labels):
         fc_events, fc_features = factual_seeds
         max_len = len(fc_events)
-        for i in enumerate(range(max_len)):
-            yield fc_events[i], fc_features[i]
+        for i in range(max_len):
+            yield (fc_events[i][None, ...], fc_features[i][None, ...]), labels[i]
 
     @abstractmethod
-    def initialize_population(self, *args, **kwargs):
+    def _init_population(self, fc_seed, **kwargs):
         pass
 
     @abstractmethod
@@ -67,9 +66,6 @@ class EvolutionaryStrategy(BaseModelMixin, ABCMeta):
     def _generate_population(self, cf_offspring, fc_seed, **kwargs):
         pass
 
-    @abstractmethod
-    def _init_population(self, fc_seed, **kwargs):
-        pass
 
     @abstractmethod
     def _recombine_parents(self, events, features, *args, **kwargs):
@@ -92,3 +88,7 @@ class EvolutionaryStrategy(BaseModelMixin, ABCMeta):
 
     def __is_termination(self, *args, **kwargs):
         return self.evolutionary_counter >= self.max_iter
+
+    # @abstractmethod
+    # def __call__(self, *args, **kwargs):
+    #     pass
--- src/thesis_generators/models/evolutionary_strategies/unrestricted_search.py
+++ src/thesis_generators/models/evolutionary_strategies/unrestricted_search.py
@@ -1,16 +1,33 @@
 from thesis_viability.viability.viability_function import ViabilityMeasure
 from thesis_generators.models.evolutionary_strategies.skeleton import EvolutionaryStrategy
+import io
+import os
 import numpy as np
+from thesis_commons.functions import reverse_sequence_2
+from thesis_viability.viability.viability_function import ViabilityMeasure
+from thesis_commons.functions import stack_data
+from thesis_commons.constants import PATH_MODELS_PREDICTORS, PATH_MODELS_GENERATORS
+import thesis_commons.metric as metric
+from thesis_readers import OutcomeBPIC12Reader as Reader
+from thesis_commons.modes import DatasetModes, FeatureModes
+from thesis_commons.modes import TaskModes
+from thesis_generators.models.encdec_vae.vae_seq2seq import SimpleGeneratorModel as Generator
+import tensorflow as tf
+import pandas as pd
+import glob
+from thesis_predictors.models.lstms.lstm import OutcomeLSTM
+
+DEBUG = True
 
 
-class UnrestricatedEvStrategy(EvolutionaryStrategy):
+class SimpleEvolutionStrategy(EvolutionaryStrategy):
     def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
 
     def _init_population(self, fc_seed, **kwargs):
         fc_ev, fc_ft = fc_seed
-        random_events = np.random.randint(0, self.vocab_len, fc_ev.shape)
-        random_features = np.random.randn(0, self.vocab_len, fc_ft.shape)
+        random_events = np.random.randint(0, self.vocab_len, (self.num_population, ) + fc_ev.shape[1:])
+        random_features = np.random.standard_normal((self.num_population, ) + fc_ft.shape[1:])
         return random_events, random_features
 
     def _recombine_parents(self, events, features, total, *args, **kwargs):
@@ -25,8 +42,9 @@ class UnrestricatedEvStrategy(EvolutionaryStrategy):
         child_features[gene_flips] = father_features[gene_flips]
         return child_events, child_features
 
-    def _mutate_offspring(self, cf_offspring, factual_seed, *args, **kwargs):
+    def _mutate_offspring(self, cf_offspring, fc_seed, *args, **kwargs):
         cf_ev, cf_ft = cf_offspring
+        return self._mutate_events(cf_ev, cf_ft)
 
     def _mutate_events(self, events, features, mutation_rate, *args, **kwargs):
         # mutation_selection = np.random.random([events.shape[0]]) > mutation_rate
@@ -38,33 +56,32 @@ class UnrestricatedEvStrategy(EvolutionaryStrategy):
         features[m_type == 0] = 0
         # CHANGE
         events[m_type == 1] = np.random.randint(1, self.vocab_len, events.shape)[m_type == 1]
-        features[m_type == 1] = np.random.randn(features.shape)[m_type == 1]
+        features[m_type == 1] = np.random.standard_normal(features.shape)[m_type == 1]
         # INSERT
         events[(m_type == 2) & (events == 0)] = np.random.randint(1, self.vocab_len, events.shape)[(m_type == 2) & (events == 0)]
-        features[m_type == 2 & (events == 0)] = np.random.randn(features.shape)[m_type == 2 & (events == 0)]
+        features[m_type == 2 & (events == 0)] = np.random.standard_normal(features.shape)[m_type == 2 & (events == 0)]
         # SWAP
         swap_mask = (m_type == 3) & (np.random.random([events.shape[0]]) > 0.1)
-        
+
         source_container = np.roll(events, -1, axis=1)
         tmp_container = np.ones_like(events) * np.nan
         tmp_container[swap_mask] = events[swap_mask]
         tmp_container = np.roll(source_container, 1, axis=1)
         backswap_mask = ~np.isnan(tmp_container)
-        
+
         events[swap_mask] = source_container[swap_mask]
         events[backswap_mask] = tmp_container[backswap_mask]
-        
+
         tmp_container = np.ones_like(features) * np.nan
         tmp_container[swap_mask] = events[swap_mask]
         tmp_container = np.roll(source_container, 1, axis=1)
-        
+
         features[swap_mask] = source_container[swap_mask]
         features[backswap_mask] = tmp_container[backswap_mask]
-        
-        return events, features
-    
 
-    def _generate_population(self, cf_parents, factual_seed, **kwargs):
+        return events, features
+
+    def _generate_population(self, cf_parents, fc_seed, **kwargs):
         cf_ev, cf_ft = cf_parents
         offspring = self._recombine_parents(cf_ev, cf_ft, self.num_population)
         return offspring
@@ -74,3 +91,34 @@ class UnrestricatedEvStrategy(EvolutionaryStrategy):
         fc_ev, fc_ft = fc_seed
         fitness = self.fitness_function(fc_ev, fc_ft, cf_ev, cf_ft)
         return fitness
+
+
+DEBUG = True
+if __name__ == "__main__":
+    task_mode = TaskModes.OUTCOME_PREDEFINED
+    epochs = 50
+    reader = Reader(mode=task_mode).init_meta(skip_dynamics=True)
+    custom_objects_predictor = {obj.name: obj for obj in OutcomeLSTM.init_metrics()}
+    custom_objects_generator = {obj.name: obj for obj in Generator.get_loss_and_metrics()}
+
+    # generative_reader = GenerativeDataset(reader)
+    (tr_events, tr_features), _, _ = reader._generate_dataset(data_mode=DatasetModes.TRAIN, ft_mode=FeatureModes.FULL_SEP)
+    (fa_events, fa_features), fa_labels, _ = reader._generate_dataset(data_mode=DatasetModes.TEST, ft_mode=FeatureModes.FULL_SEP)
+    fa_events, fa_features, fa_labels = fa_events[:1], fa_features[:1], fa_labels[:1, 0]
+
+    all_models_predictors = os.listdir(PATH_MODELS_PREDICTORS)
+    predictor = tf.keras.models.load_model(PATH_MODELS_PREDICTORS / all_models_predictors[-1], custom_objects=custom_objects_predictor)
+    print("PREDICTOR")
+    predictor.summary()
+
+    viability = ViabilityMeasure(reader.vocab_len, reader.max_len, (tr_events, tr_features), predictor)
+
+    generator = SimpleEvolutionStrategy(
+        evaluator=viability,
+        vocab_len=reader.vocab_len,
+        max_len=reader.max_len,
+        feature_len=reader.current_feature_len,
+    )
+
+    final_population, final_fitness = generator([fa_events, fa_features], fa_labels)
+    print("DONE")
