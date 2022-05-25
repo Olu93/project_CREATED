cbad6e5d9bb1145ee5de8a3dcf828dd87ad664c5
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-05-25 02:24:05 +0200

    Still trying to solve imports

--- src/thesis_commons/libcuts.py
+++ src/thesis_commons/libcuts.py
@@ -2,8 +2,14 @@ import tensorflow as tf
 # import keras.api._v2.keras as keras
 # K = backend
 from tensorflow import keras
-from tensorflow.keras import  optimizers, layers, models, losses as ls, metrics, utils
+from tensorflow.keras import optimizers, layers, models, losses as ls, metrics, utils
 from tensorflow.python.keras import backend as K
 # from keras.api._v2.keras import optimizers, layers, models, losses as ls, metrics, utils
 
-losses = ls
+losses = tf.keras.losses
+optimizers = tf.keras.optimizers
+models = tf.keras.models
+metrics = tf.keras.metrics
+utils = tf.keras.utils
+
+
