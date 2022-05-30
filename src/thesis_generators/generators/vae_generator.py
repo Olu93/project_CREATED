999c7616469e817d0425a3da6e9d07d99e4246e1
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-05-30 23:41:06 +0200

    Fix: Minor bug from predicting with generator instead of predictor

--- src/thesis_generators/generators/vae_generator.py
+++ src/thesis_generators/generators/vae_generator.py
@@ -17,7 +17,7 @@ class SimpleVAEGenerator(GeneratorMixin):
         fa_events, fa_features = fc_case.data
         fa_ev_rep, fa_ft_rep = np.repeat(fa_events, 10, axis=0), np.repeat(fa_features, 10, axis=0)
         (cf_ev, cf_ft) = self.generator.predict((fa_ev_rep, fa_ft_rep))
-        outcomes = self.generator.predict((fa_events, fa_features))
+        outcomes = self.predictor.predict((fa_events, fa_features))
         viabilities = self.evaluator(fa_events, fa_features, cf_ev, cf_ft)
         return cf_ev, cf_ft, outcomes, viabilities
     
