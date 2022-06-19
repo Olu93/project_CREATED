caa889fd02193d25182c6995620294a2fe3c79fa
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-06-20 01:51:57 +0200

    Chkpt: About to make breaking changes

--- src/thesis_readers/helper/preprocessing.py
+++ src/thesis_readers/helper/preprocessing.py
@@ -4,6 +4,11 @@ from collections import UserList
 from typing import Callable, Dict, List
 import pandas as pd
 
+from thesis_commons.representations import BetterDict
+
+
+
+
 
 class Operation(ABC):
     name = None
@@ -11,19 +16,33 @@ class Operation(ABC):
     o_cols = None
     _children: List[Operation] = None
     _parents: List[Operation] = None
-    _params: Dict = None
-    _params_r: Dict = None
+    _params: BetterDict = None
+    _params_r: BetterDict = None#
 
-    def __init__(self, forward: Callable, backward: Callable, **kwargs):
+    def __init__(self, forward: Callable, backward: Callable, **kwargs:BetterDict):
         self.forward = forward
         self.backward = backward
+        self._params = kwargs or BetterDict()
+
+    def set_params(self, **kwargs)-> Operation:
+        self._params = kwargs
+        return self
+
+    def add_child(self, child: Operation)-> Operation:
+        self._children.append(child)
+        return self
+
+    def add_parent(self, parent: Operation)-> Operation:
+        self._parents.append(parent)
+        return self
 
     @abstractmethod
     def process(self, inputs, **kwargs):
-        self._params = kwargs
-        result, params_r = self.forward(inputs, **kwargs)
+        self._params = self._params.merge(kwargs)
+        result, params_r = self.forward(inputs, **self._params)
         self._params_r = params_r
-        return result
+        self.result = result
+        return self
 
     @abstractmethod
     def revert(self, inputs, **kwargs):
@@ -41,8 +60,19 @@ class IrreversableOperation(Operation):
     pass
 
 class DropOperation(IrreversableOperation):
-    pass
-
+    def __init__(self, forward: Callable, backward: Callable=None, **kwargs):
+        super().__init__(forward, backward, **kwargs)
+    
+    def drop(self, data: pd.DataFrame, cols=None):
+        self._params = {"cols":cols}
+        if not cols:
+            self.new_data = data
+            return None
+        new_data = data.drop(cols, axis=1)
+        # removed_cols = set(data.columns) - set(new_data.columns)
+        self.new_data = new_data
+        self._params_r = new_data.columns
+        
 
 class ProcessingPipeline(UserList):
     pass
