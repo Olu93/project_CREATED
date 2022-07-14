0b5bcb0c9cfbeeffc6457ebc92ddd2dd459f7fd1
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-07-14 21:39:32 +0200

    Ref: Small

--- src/thesis_experiments/run_training_readers.py
+++ src/thesis_experiments/run_training_readers.py
@@ -1,14 +1,17 @@
 from thesis_commons.modes import TaskModes
 from thesis_readers import OutcomeMockReader, OutcomeBPIC12ReaderShort, OutcomeBPIC12ReaderMedium, OutcomeBPIC12ReaderFull
 
+DEBUG_SKIP_VIZ = True
+DEBUG_SKIP_STATS = False
+
 if __name__ == '__main__':
     # TODO: Put debug stuff into configs
     save_preprocessed = True
-    reader = OutcomeMockReader(debug=True, mode=TaskModes.OUTCOME_PREDEFINED).init_log(save_preprocessed).init_meta(False)
-    reader.save(True)
-    reader = OutcomeBPIC12ReaderShort(debug=True, mode=TaskModes.OUTCOME_PREDEFINED).init_log(save_preprocessed).init_meta(False)
-    reader.save(True)
-    reader = OutcomeBPIC12ReaderMedium(debug=True, mode=TaskModes.OUTCOME_PREDEFINED).init_log(save_preprocessed).init_meta(False)
-    reader.save(True)
-    reader = OutcomeBPIC12ReaderFull(debug=True, mode=TaskModes.OUTCOME_PREDEFINED).init_log(save_preprocessed).init_meta(False)
-    reader.save(True)
+    reader = OutcomeMockReader(debug=False, mode=TaskModes.OUTCOME_PREDEFINED).init_log(save_preprocessed).init_meta(False)
+    reader.save(skip_viz=DEBUG_SKIP_VIZ,skip_stats=DEBUG_SKIP_STATS)
+    reader = OutcomeBPIC12ReaderShort(debug=False, mode=TaskModes.OUTCOME_PREDEFINED).init_log(save_preprocessed).init_meta(False)
+    reader.save(skip_viz=DEBUG_SKIP_VIZ,skip_stats=DEBUG_SKIP_STATS)
+    reader = OutcomeBPIC12ReaderMedium(debug=False, mode=TaskModes.OUTCOME_PREDEFINED).init_log(save_preprocessed).init_meta(False)
+    reader.save(skip_viz=DEBUG_SKIP_VIZ,skip_stats=DEBUG_SKIP_STATS)
+    reader = OutcomeBPIC12ReaderFull(debug=False, mode=TaskModes.OUTCOME_PREDEFINED).init_log(save_preprocessed).init_meta(False)
+    reader.save(skip_viz=DEBUG_SKIP_VIZ,skip_stats=DEBUG_SKIP_STATS)
