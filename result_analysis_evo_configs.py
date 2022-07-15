7141eb40512c9baaf8f7dea148f6c62a49622821
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-07-15 08:19:25 +0200

    Send to Xixi

--- result_analysis_evo_configs.py
+++ result_analysis_evo_configs.py
@@ -6,12 +6,16 @@ import pathlib
 import seaborn as sns
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
+from io import StringIO
+
 # https://support.minitab.com/en-us/minitab/18/help-and-how-to/modeling-statistics/anova/how-to/mixed-effects-model/interpret-the-results/key-results/
 # %%
 PATH = pathlib.Path('results/models_overall/evolutionary_configs/experiment_evolutionary_configs_results.csv')
 original_df = pd.read_csv(PATH)
 original_df.head()
 # %%
+full_name_str = "Configuration Set"
+
 exog = {
     "gen.initiator.type": "initiator",
     "gen.selector.type": "selector",
@@ -22,7 +26,7 @@ exog = {
 
 renaming = {
     "run.short_name": "short_name",
-    "run.full_name": "full_name",
+    "run.full_name": full_name_str,
 }
 
 df = original_df.rename(columns=exog)
@@ -31,21 +35,81 @@ df = df.rename(columns=renaming)
 # sm.GLS(original_df["viability"], original_df[exog])
 # %%
 formular_exog = " + ".join(exog.values())
-md = smf.mixedlm(f"viability ~ {formular_exog}", df, groups="full_name")
-mdf = md.fit()
-mdf.summary()
+md1 = smf.mixedlm(f"viability ~ {formular_exog}", df, groups=full_name_str)
+mdf1 = md1.fit()
+mdf1.summary()
 # %%
 # https://github.com/statsmodels/statsmodels/issues/6157
-print(md.score(mdf.params_object))
-mdf.params
+print(md1.score(mdf1.params_object))
+mdf1.params
 # %%
 formular_exog = " + ".join(exog.values())
-md = smf.mixedlm(f"dllh ~ {formular_exog} ", df, groups="full_name")
-mdf = md.fit(method="cg")
-mdf.summary()
+md2 = smf.mixedlm(f"feasibility ~ {formular_exog} ", df, groups=full_name_str)
+mdf2 = md2.fit(method="cg")
+mdf2.summary()
 # %%
 # https://github.com/statsmodels/statsmodels/issues/6157
-print(md.score(mdf.params_object))
-mdf.params# %%
+print(md2.score(mdf2.params_object))
+mdf2.params  # %%
+
+# %%
+base1, specific1 = mdf1.summary().tables
+base2, specific2 = mdf2.summary().tables
+# %%
+specific1_mod = specific1.iloc[:, :-2].drop('z', axis=1).rename(columns={'P>|z|': 'p-value'})
+specific2_mod = specific2.iloc[:, :-2].drop('z', axis=1).rename(columns={'P>|z|': 'p-value'})
+fmt = {
+   ("Numeric", "Integers"): '\${}',
+   ("Numeric", "Floats"): '{:.6f}',
+   ("Non-Numeric", "Strings"): str.upper
+}
+
+# %%
+caption = StringIO()
+caption.write("Table shows the result of the linear mixed model.")
+caption.write(" ")
+caption.write("It uses viability as dependent variable and evolutionary operators as independent categorical variable.")
+caption.write(" ")
+caption.write("The model is adjusted for general differences in combinations.")
+caption.write(" ")
+caption.write("The coefficient explains the effect of the model, while p-value explains the statistical significance.")
+caption.write(" ")
+caption.write("Only significant values are considerable effects.")
+
+result_table_latex = specific1_mod.style.format(fmt).to_latex(
+    caption=caption.getvalue(),
+    label="tbl:configs_viability",
+)
+
+print(result_table_latex)
+
+# %%
+caption = StringIO()
+caption.write("Table shows the result of the linear mixed model.")
+caption.write(" ")
+caption.write("It uses feasibility as dependent variable and evolutionary operators as independent categorical variable.")
+caption.write(" ")
+caption.write("The model is adjusted for general differences in combinations.")
+caption.write(" ")
+caption.write("The coefficient explains the effect of the model, while the p-value explains the statistical significance.")
+caption.write(" ")
+caption.write("Only significant values are considerable effects.")
+
+result_table_latex = specific2_mod.style.format(escape='latex').to_latex(
+    caption=caption.getvalue(),
+    label="tbl:configs_feasibility",
+)
+
+print(result_table_latex)
+
+# %%
+# result_table_better = mdf1.summary()
+# result_table_latex = result_table_better.as_latex(
+#     # caption=
+#     # "",
+#     # label="tbl:configs_1",
+# )
+
+# print(result_table_latex)
 
 # %%
--- result_analysis_evo_params.py
+++ result_analysis_evo_params.py
@@ -6,6 +6,7 @@ import pathlib
 import seaborn as sns
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
+from io import StringIO
 # https://support.minitab.com/en-us/minitab/18/help-and-how-to/modeling-statistics/anova/how-to/mixed-effects-model/interpret-the-results/key-results/
 # %%
 PATH = pathlib.Path('results/models_overall/evolutionary_params/experiment_evolutionary_params_results.csv')
@@ -20,15 +21,16 @@ exog = {
     "gen.crosser.type": "crosser",
 }
 
+full_name_str = "Hyperparameter Set"
 renaming = {
     "run.short_name": "short_name",
-    "run.full_name": "full_name",
-    "gen.mutator.p_delete": "dmrate",
-    "gen.mutator.p_insert": "imrate",
-    "gen.mutator.p_change": "cmrate",
-    "gen.mutator.p_transp": "tmrate",
-    "gen.mutator.p_none": "nmrate",
-    "gen.mutator.edit_rate": "erate",
+    "run.full_name": full_name_str,
+    "gen.mutator.p_delete": "delete",
+    "gen.mutator.p_insert": "insert",
+    "gen.mutator.p_change": "change",
+    "gen.mutator.p_transp": "transp",
+    "gen.mutator.p_none": "nochng",
+    "gen.mutator.edit_rate": "editrate",
     "gen.crosser.crossover_rate": "crate",
     "gen.recombiner.recombination_rate": "rrate",
     "run.duration_sec": "duration",
@@ -44,22 +46,74 @@ df = df.rename(columns=renaming)
 # sm.GLS(original_df["viability"], original_df[exog])
 # %%
 formular_exog = " + ".join(exog.values())
-md = smf.mixedlm(f"viability ~ erate + dmrate + imrate + cmrate + tmrate + {formular_exog}", df, groups="short_name")
-mdf = md.fit()
-mdf.summary()
+md1 = smf.mixedlm(f"viability ~ editrate + editrate:delete + editrate:insert + editrate:change + editrate:transp", df, groups=full_name_str)
+mdf1 = md1.fit()
+mdf1.summary()
 # %%
 # https://github.com/statsmodels/statsmodels/issues/6157
-print(md.score(mdf.params_object))
-mdf.params
+print(md1.score(mdf1.params_object))
+mdf1.params
 # %%
 formular_exog = " + ".join(exog.values())
-md = smf.mixedlm(f"feasibility ~ erate + dmrate + imrate + cmrate + tmrate + {formular_exog}", df, groups="short_name")
-mdf = md.fit()
-mdf.summary()
+md2 = smf.mixedlm(f"viability ~ editrate + editrate:delete + editrate:insert + editrate:change + editrate:transp", df, groups=full_name_str)
+mdf2 = md2.fit()
+mdf2.summary()
 # %%
 # https://github.com/statsmodels/statsmodels/issues/6157
-print(md.score(mdf.params_object))
-mdf.params
+print(md2.score(mdf2.params_object))
+mdf2.params
+
+# %%
+base1, specific1 = mdf1.summary().tables
+base2, specific2 = mdf2.summary().tables
+# %%
+specific1_mod = specific1.iloc[:, :-2].drop('z', axis=1).rename(columns={'P>|z|': 'p-value'})
+specific2_mod = specific2.iloc[:, :-2].drop('z', axis=1).rename(columns={'P>|z|': 'p-value'})
+fmt = {
+   ("Numeric", "Integers"): '\${}',
+   ("Numeric", "Floats"): '{:.6f}',
+   ("Non-Numeric", "Strings"): str.upper
+}
+# %%
+caption = StringIO()
+caption.write("Table shows the result of the linear mixed model.")
+caption.write(" ")
+caption.write("It uses viability as dependent variable and the hyperparameters as independent numerical variables.")
+caption.write(" ")
+caption.write("The model is adjusted for general differences in indivdual hyperparameter settings.")
+
+result_table_latex = specific1_mod.style.format(fmt).to_latex(
+    caption=caption.getvalue(),
+    label="tbl:params_viability",
+)
+
+print(result_table_latex)
+
+# %%
+caption = StringIO()
+caption.write("Table shows the result of the linear mixed model.")
+caption.write(" ")
+caption.write("It uses feasibility as dependent variable and the hyperparameters as independent numerical variables.")
+caption.write(" ")
+caption.write("The model is adjusted for general differences in indivdual hyperparameter settings.")
+
+result_table_latex = specific2_mod.style.format(fmt).to_latex(
+    caption=caption.getvalue(),
+    label="tbl:params_feasibility",
+)
+
+print(result_table_latex)
+
+# %%
+fig, ax = plt.subplots(figsize=(10,10))
+sns.lineplot(data=df, x='delete', y='viability', ax=ax)
+sns.lineplot(data=df, x='insert', y='viability', ax=ax)
+sns.lineplot(data=df, x='change', y='viability', ax=ax)
+sns.lineplot(data=df, x='transp', y='viability', ax=ax)
+ax.set_xlabel('All hyperparams except editrate')
+plt.show()
+
+
 # %%
 df_only_feasible = df[df['feasibility'] > 0]
 formular_exog = " + ".join(exog.values())
