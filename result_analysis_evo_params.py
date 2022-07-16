25e3b6217b2dddeced2540745d553db1c8469138
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-07-16 22:41:28 +0200

    Chkpt: Changed the sample search to compute quicker

--- result_analysis_evo_params.py
+++ result_analysis_evo_params.py
@@ -14,6 +14,8 @@ PATH = pathlib.Path('results/models_overall/evolutionary_params/experiment_evolu
 original_df = pd.read_csv(PATH)
 original_df.head()
 # %%
+
+
 exog = {
     "gen.initiator.type": "initiator",
     "gen.selector.type": "selector",
@@ -22,27 +24,34 @@ exog = {
     "gen.crosser.type": "crosser",
 }
 
-full_name_str = "Hyperparameter Set"
-renaming = {
-    "run.short_name": "short_name",
-    "run.full_name": full_name_str,
+edit_types = {
     "gen.mutator.p_delete": "delete",
     "gen.mutator.p_insert": "insert",
     "gen.mutator.p_change": "change",
     "gen.mutator.p_transp": "transp",
     "gen.mutator.p_none": "nochng",
-    "gen.mutator.edit_rate": "editrate",
+}
+
+edit_rate = {
+        "gen.mutator.edit_rate": "editrate",
+}
+
+full_name_str = "Hyperparameter Set"
+renaming = {
+    "run.short_name": "short_name",
+    "run.full_name": full_name_str,
     "gen.crosser.crossover_rate": "crate",
     "gen.recombiner.recombination_rate": "rrate",
     "run.duration_sec": "duration",
 }
 
-cols_of_interest = ["feasibility", "viability", "erate", "dmrate", "imrate", "cmrate", "tmrate", "nmrate"]
-cols_config = ["initiator", "selector", "mutator", "crosser", "recombiner"]
+cols_dependent = ["feasibility", "viability"]
+cols_editrate = list(edit_rate.values())
+cols_edittypes = list(edit_types.values())
+cols_config = list(exog.values())
 
 df = original_df.copy()
-df = df.rename(columns=exog)
-df = df.rename(columns=renaming)
+df = df.rename(columns=exog).rename(columns=edit_types).rename(columns=edit_rate).rename(columns=renaming)
 
 # sm.GLS(original_df["viability"], original_df[exog])
 # %%
@@ -56,7 +65,7 @@ print(md1.score(mdf1.params_object))
 mdf1.params
 # %%
 formular_exog = " + ".join(exog.values())
-md2 = smf.mixedlm(f"viability ~ editrate + editrate:delete + editrate:insert + editrate:change + editrate:transp", df, groups=full_name_str)
+md2 = smf.mixedlm(f"feasibility ~ editrate + editrate:delete + editrate:insert + editrate:change + editrate:transp", df, groups=full_name_str)
 mdf2 = md2.fit()
 mdf2.summary()
 # %%
@@ -102,110 +111,101 @@ result_table_latex = specific2_mod.style.format(fmt).to_latex(
 print(result_table_latex)
 
 # %%
-fig, ax = plt.subplots(figsize=(10, 10))
-sns.lineplot(data=df, x='delete', y='viability', ax=ax)
-sns.lineplot(data=df, x='insert', y='viability', ax=ax)
-sns.lineplot(data=df, x='change', y='viability', ax=ax)
-sns.lineplot(data=df, x='transp', y='viability', ax=ax)
-ax.set_xlabel('All hyperparams except editrate')
-plt.show()
-# %% plot
-sm = pd.melt(df, id_vars=[
-    "feasibility",
-    "viability",
-    "rank",
-], value_vars=["delete", "insert", "change", "transp", "nochng"])
-sm
-# %%
-df_only_feasible = df[df['feasibility'] > 0]
-formular_exog = " + ".join(exog.values())
-md = smf.mixedlm(f"viability ~ erate + dmrate + imrate + cmrate + tmrate + {formular_exog}", df_only_feasible, groups="short_name")
-mdf = md.fit()
-mdf.summary()
-# %%
-# https://github.com/statsmodels/statsmodels/issues/6157
-print(md.score(mdf.params_object))
-mdf.params  # %%
-
-# %%
-df_only_feasible = df[df['feasibility'] > 0]
-formular_exog = " + ".join(exog.values())
-md = smf.mixedlm(f"feasibility ~ erate + dmrate + imrate + cmrate + tmrate + {formular_exog}", df_only_feasible, groups="short_name")
-mdf = md.fit()
-mdf.summary()
-# %%
-# https://github.com/statsmodels/statsmodels/issues/6157
-print(md.score(mdf.params_object))
-mdf.params  # %%
-
-# %%
-df_high_feasible = df[df['feasibility'] > 0.1]
-df_high_feasible.groupby(['short_name']).mean()[cols_of_interest].mean()
-# %%
-df.describe()[cols_of_interest]
-# %%
-df[df['feasibility'] > 0.1].describe()[cols_of_interest]
-# %%
-df[df['feasibility'] > 0.2].describe()[cols_of_interest]
-# %%
-best_values = df[df.viability == df.viability.max()]
-best_values
-# %%
-best_values[cols_config + cols_of_interest]
-# %%
-df["is_feasible"] = df['feasibility'] > 0
-most_feasible = df.groupby('short_name').sum()["is_feasible"].sort_values().tail()
-most_feasible
-# %%
-best_configs_to_avoid_zeros = df[df["short_name"].isin(most_feasible.index)]
-best_configs_to_avoid_zeros
-# %%
-best_configs_to_avoid_zeros.groupby(cols_config).mean()[cols_of_interest]
-# %%
-
-tmp0 = df[df['feasibility'] > 0.0].describe()[cols_of_interest].loc["50%"]
-tmp1 = df[df['feasibility'] > 0.1].describe()[cols_of_interest].loc["50%"]
-tmp2 = df[df['feasibility'] > 0.2].describe()[cols_of_interest].loc["50%"]
-tmpa = best_configs_to_avoid_zeros.describe()[cols_of_interest].loc["50%"]
-# %%
-pd.DataFrame([tmp0, tmp1, tmp2, tmpa]).T
-# %%
-most_feasible
-# %%
-best_values
-# %%
-rename_vals = {
-    "viability": "viability",
-    "feasibility": "feasibility",
-    "dmrate": "delete-rate",
-    "imrate": "insert-rate",
-    "cmrate": "change-rate",
-    "tmrate": "transp-rate",
-    "nmrate": "nochng-rate",
-    "erate": "edit-rate",
-}
-corr_matrix = df[cols_of_interest].rename(columns=rename_vals)[rename_vals.values()].corr()
-plt.figure(figsize=(10, 7))
-
-sns.heatmap(corr_matrix, annot=True, fmt=".3f")
-plt.savefig('latex/thesis_phase_2/figures/results/params_heatmap.png')
+fig, ax = plt.subplots((4), figsize=(10, 15))
+df_avg_over_editrate = df.groupby(cols_edittypes + ["initiator"]).mean().reset_index()
+sns.lineplot(data=df_avg_over_editrate, x='delete', y='viability', ax=ax[0], hue="initiator")
+sns.lineplot(data=df_avg_over_editrate, x='insert', y='viability', ax=ax[1], hue="initiator")
+sns.lineplot(data=df_avg_over_editrate, x='change', y='viability', ax=ax[2], hue="initiator")
+sns.lineplot(data=df_avg_over_editrate, x='transp', y='viability', ax=ax[3], hue="initiator")
+# ax.set_xlabel('All hyperparams except editrate')
 plt.show()
 # %%
-# df_agg = df.groupby("rank").mean()
-sns.lineplot(df, x='rank', y="viability", hue="")
+fig, ax = plt.subplots(figsize=(10, 15))
+df_avg_over_selectors = df.groupby(cols_editrate+["initiator"]).mean().reset_index()
+sns.lineplot(data=df_avg_over_selectors, x=cols_editrate[0], y='viability', ax=ax, hue="initiator")
+
+# ax.set_xlabel('All hyperparams except editrate')
+plt.show()
 # %%
-# df.groupby("rank")
-melted = pd.melt(
-    df,
-    value_name= "rate",
-    value_vars=["delete", "insert", "change", "transp"],
-    id_vars=["viability", "rank", "iteration.no", "feasibility"],
-)
-melted = melted.groupby(["variable","iteration.no", "rank"]).mean().groupby(["iteration.no", "rank"]).mean()
-melted
-# sns.lineplot(melted, x = 'value',  hue="variable")
+
+# sm = pd.melt(df, id_vars=[
+#     "feasibility",
+#     "viability",
+#     "rank",
+# ], value_vars=["delete", "insert", "change", "transp", "nochng"])
+# sm
+
+
+# # %%
+# df_high_feasible = df[df['feasibility'] > 0.1]
+# df_high_feasible.groupby(['short_name']).mean()[cols_of_interest].mean()
+# # %%
+# df.describe()[cols_of_interest]
+# # %%
+# df[df['feasibility'] > 0.1].describe()[cols_of_interest]
+# # %%
+# df[df['feasibility'] > 0.2].describe()[cols_of_interest]
+# # %%
+# best_values = df[df.viability == df.viability.max()]
+# best_values
+# # %%
+# best_values[cols_config + cols_of_interest]
+# # %%
+# df["is_feasible"] = df['feasibility'] > 0
+# most_feasible = df.groupby('short_name').sum()["is_feasible"].sort_values().tail()
+# most_feasible
+# # %%
+# best_configs_to_avoid_zeros = df[df["short_name"].isin(most_feasible.index)]
+# best_configs_to_avoid_zeros
+# # %%
+# best_configs_to_avoid_zeros.groupby(cols_config).mean()[cols_of_interest]
+# # %%
+
+# tmp0 = df[df['feasibility'] > 0.0].describe()[cols_of_interest].loc["50%"]
+# tmp1 = df[df['feasibility'] > 0.1].describe()[cols_of_interest].loc["50%"]
+# tmp2 = df[df['feasibility'] > 0.2].describe()[cols_of_interest].loc["50%"]
+# tmpa = best_configs_to_avoid_zeros.describe()[cols_of_interest].loc["50%"]
+# # %%
+# pd.DataFrame([tmp0, tmp1, tmp2, tmpa]).T
+# # %%
+# most_feasible
+# # %%
+# best_values
+# # %%
+# rename_vals = {
+#     "viability": "viability",
+#     "feasibility": "feasibility",
+#     "delte": "delete-rate",
+#     "imrate": "insert-rate",
+#     "cmrate": "change-rate",
+#     "tmrate": "transp-rate",
+#     "nmrate": "nochng-rate",
+#     "erate": "edit-rate",
+# }
+# corr_matrix = df[cols_of_interest].rename(columns=rename_vals)[rename_vals.values()].corr()
+# plt.figure(figsize=(10, 7))
+
+# sns.heatmap(corr_matrix, annot=True, fmt=".3f")
+# plt.savefig('latex/thesis_phase_2/figures/results/params_heatmap.png')
+# plt.show()
+# # %%
+# # df_agg = df.groupby("rank").mean()
+# sns.lineplot(df, x='rank', y="viability", hue="")
+# # %%
+# # df.groupby("rank")
+# melted = pd.melt(
+#     df,
+#     value_name= "rate",
+#     value_vars=["delete", "insert", "change", "transp"],
+#     id_vars=["viability", "rank", "iteration.no", "feasibility"],
+# )
+# melted = melted.groupby(["variable","iteration.no", "rank"]).mean().groupby(["iteration.no", "rank"]).mean()
 # melted
-# %%
-sns.lineplot(data=melted, x='rank', y="feasibility", hue="variable")
+# # sns.lineplot(melted, x = 'value',  hue="variable")
+# # melted
+# # %%
+# sns.lineplot(data=melted, x='rank', y="feasibility", hue="variable")
+
+# # %%
 
 # %%
