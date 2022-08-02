fd7242275a0707349b60353cb879c92c3bb3d4c9
Olusanmi Hundogan <o.hundogan@gmail.com>
2022-08-02 04:49:23 +0200

    Added

--- src/thesis_experiments/run_experiment_evolutionary_iterations.py
+++ src/thesis_experiments/run_experiment_evolutionary_iterations.py
@@ -12,7 +12,7 @@ from keras import models
 from tqdm import tqdm
 import time
 from thesis_commons.random import random
-from thesis_commons.config import DEBUG_USE_MOCK, MAX_ITER_STAGE_SPECIAL
+from thesis_commons.config import DEBUG_USE_MOCK, MAX_ITER_STAGE_SPECIAL, READER
 from thesis_commons.constants import (PATH_MODELS_GENERATORS, PATH_MODELS_PREDICTORS, PATH_RESULTS_MODELS_OVERALL, PATH_RESULTS_MODELS_SPECIFIC)
 from thesis_commons.distributions import DataDistribution, DistributionConfig
 from thesis_commons.model_commons import GeneratorWrapper, TensorflowModelMixin
@@ -55,6 +55,7 @@ def create_combinations(erate: float, mrate: MutationRate, evaluator: ViabilityM
     mutators = [evolutionary_operations.SamplingBasedMutator().set_data_distribution(evaluator.measures.dllh.data_distribution).set_mutation_rate(mrate).set_edit_rate(erate)]
     recombiners = [
         evolutionary_operations.FittestSurvivorRecombiner(),
+        evolutionary_operations.BestBreedRecombiner(),
     ]
     combos = it.product(initiators, selectors, crossers, mutators, recombiners)
     return combos
@@ -72,7 +73,7 @@ if __name__ == "__main__":
     experiment_name = "evolutionary_iterations"
     outcome_of_interest = None
     
-    ds_name = "OutcomeBPIC12Reader25"
+    ds_name = READER
     reader:AbstractProcessLogReader = AbstractProcessLogReader.load(PATH_READERS / ds_name)
     predictor: TensorflowModelMixin = models.load_model(PATH_MODELS_PREDICTORS / ds_name.replace('Reader', 'Predictor'), compile=False)
     print("PREDICTOR")
--- src/thesis_experiments/run_experiment_evolutionary_sidequest_configs.py
+++ src/thesis_experiments/run_experiment_evolutionary_sidequest_configs.py
@@ -11,7 +11,7 @@ keras = tf.keras
 from keras import models
 from tqdm import tqdm
 import time
-from thesis_commons.config import DEBUG_USE_MOCK, MAX_ITER_STAGE_1
+from thesis_commons.config import DEBUG_USE_MOCK, MAX_ITER_STAGE_1, READER
 from thesis_commons.constants import (PATH_MODELS_GENERATORS, PATH_MODELS_PREDICTORS, PATH_RESULTS_MODELS_OVERALL, PATH_RESULTS_MODELS_SPECIFIC)
 from thesis_commons.distributions import DataDistribution, DistributionConfig
 from thesis_commons.model_commons import GeneratorWrapper, TensorflowModelMixin
@@ -114,7 +114,7 @@ if __name__ == "__main__":
     experiment_name = "evolutionary_sidequest_configs"
     outcome_of_interest = None
     
-    ds_name = "OutcomeDice4ELReader"
+    ds_name = READER
     reader:AbstractProcessLogReader = AbstractProcessLogReader.load(PATH_READERS / ds_name)
     predictor: TensorflowModelMixin = models.load_model(PATH_MODELS_PREDICTORS / ds_name.replace('Reader', 'Predictor'), compile=False)
     print("PREDICTOR")
